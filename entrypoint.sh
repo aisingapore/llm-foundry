9b4756a5009fde0632f2225b41ec15488ab789c4
yier <yier@aisingapore.org>
2024-05-09 11:41:21 +0800

    split args for cli and script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,33 @@ set -euo pipefail
 
 cp -r /workspace/llm_foundry/scripts/eval/local_data $HF_DATASETS_CACHE
 
-composer /workspace/llm_foundry/scripts/eval/eval.py $@
+args=$(getopt -o c:s: -l cli:,script: -n 'm3exam' -- "$@")
+if [ $? != 0 ]; then
+    echo 'Terminating...' >&2
+    exit 1
+fi
+eval set -- "${args}"
+
+cli_args=
+script_args=
+while true; do
+    case "${1}" in
+        -c | --cli )
+            cli_args=$(echo ${2} | sed 's/,--/ --/g')
+            shift 2
+            ;;
+        -s | --script )
+            script_args=$(echo ${2} | sed 's/,--/ --/g')
+            shift 2
+            ;;
+        -- )
+            shift
+            break
+            ;;
+        * )
+            break
+            ;;
+    esac
+done
+
+composer ${cli_args} /workspace/llm_foundry/scripts/eval/eval.py ${script_args}
