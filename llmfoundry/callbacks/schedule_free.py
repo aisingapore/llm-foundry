9b34cf402d7cef173f44f1edfd53eed3fac0d080
wheynelau <waynelau15045@gmail.com>
2024-05-28 10:47:14 +0800

    refactor: remove unused logger in ScheduleFreeCallback

--- llmfoundry/callbacks/schedule_free.py
+++ llmfoundry/callbacks/schedule_free.py
@@ -10,6 +10,8 @@ class ScheduleFreeCallback(Callback):
 
     def after_load(self, state: State, logger: Logger) -> None:
         # check if there is any schedule free optimizer
+
+        del logger  # unused
         if isinstance(state.optimizer, list):
             check_optimizer = any(isinstance(optimizer, ScheduleFree) for 
                                   optimizer in state.optimizer)
@@ -23,11 +25,31 @@ class ScheduleFreeCallback(Callback):
     def fit_start(self, state: State, logger: Logger) -> None:
         # check if optimizer is AdamWScheduleFree
 
+        del logger  # unused
         if isinstance(state.optimizer, ScheduleFree):
             state.optimizer.train()
 
+        elif isinstance(state.optimizer, list):
+            for optimizer in state.optimizer:
+                if isinstance(optimizer, ScheduleFree):
+                    optimizer.train()
+
 
     def eval_start(self, state: State, logger: Logger) -> None:
-        
+
+        del logger  # unused
+
         if isinstance(state.optimizer, ScheduleFree):
             state.optimizer.eval()
+
+        elif isinstance(state.optimizer, list):
+            for optimizer in state.optimizer:
+                if isinstance(optimizer, ScheduleFree):
+                    optimizer.eval()
+
+
+    def fit_end(self, state: State, logger: Logger) -> None:
+        """
+        Copied from eval_start due to the same implementation
+        """
+        self.eval_start(state, logger)
